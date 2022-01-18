d9cf2d4ec4ba3e5fcd955538088e875e74f1dbe2
Trent M <59753390+Trent122@users.noreply.github.com>
2022-01-18 13:18:03 -0700

    Add files via upload

---
+++ MORSDK/Main.js
@@ -0,0 +1,26 @@
+const serverUrl = https//iglemelcs98c.usemoralis.com:2053/server
+const appId = Xj2l4O1crw392kUit93yKszz2H0uTlVBN8KCD9qC
+Moralis.start({ serverUrl, appId });
+
+/* Authentication code */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+    user = await Moralis.authenticate({ signingMessage: "Log in using Moralis" })
+      .then(function (user) {
+        console.log("logged in user:", user);
+        console.log(user.get("ethAddress"));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
